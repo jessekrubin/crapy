8be9c6a35285b4da1da6179290fc77a4cecd69eb
jessekrubin <jessekrubin@gmail.com>
2019-08-22 20:07:46 -0700

    crapy

--- crapy.py
+++ crapy.py
@@ -5,5 +5,10 @@ def print(string):
         sys.stdout.write(c)
     sys.stdout.write('\n')
 
-
+def add(a, b):
+    total = 0
+    for i in range(a+1):
+        for j in range(b+1):
+            total += 1
+    return total
 
